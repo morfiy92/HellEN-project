8a9eb8afb8cffb1cff26255f89fe6e4ea3349e74
Ania Walker <Ania.Wlkr@gmail.com>
2020-11-16 21:58:02 +0200

    Updates

--- header-menu.e187f57c.js
+++ header-menu.e187f57c.js
@@ -121,9 +121,11 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 (function () {
   var menuBtnRef = document.querySelector("[data-menu-button]");
   var mobileMenuRef = document.querySelector("[data-menu]");
+  var mobileLogoRef = document.querySelector("[data-menu-logo]");
   menuBtnRef.addEventListener("click", function () {
     var expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open");
+    mobileLogoRef.classList.toggle("white");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
   });
@@ -156,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49438" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59478" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
